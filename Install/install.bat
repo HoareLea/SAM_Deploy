e020155bc4748ede787d8deb915b068d3f9ea47a
Will Reynolds <willreynolds@hoarelea.com>
2020-07-02 20:55:22 +0100

    Basic installer script - GH only

--- Install/install.bat
+++ Install/install.bat
@@ -1,1 +1,5 @@
-REM todo: some logic here to copy files and install... 
+set GH_PACKAGES_DIR=%APPDATA%\Grasshopper\Libraries\SAM\
+if not exist "%GH_PACKAGES_DIR%" mkdir "%GH_PACKAGES_DIR%"
+
+copy "SAM\*.*" "%GH_PACKAGES_DIR%"
+
