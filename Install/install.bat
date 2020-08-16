e107e03a5522ec407f2d457ab0687fe227aca18d
Will Reynolds <willreynolds@hoarelea.com>
2020-08-16 05:16:06 +0100

    Install changed to appdata/SAM

--- Install/install.bat
+++ Install/install.bat
@@ -1,5 +1,29 @@
-set GH_PACKAGES_DIR=%APPDATA%\Grasshopper\Libraries\SAM\
-if not exist "%GH_PACKAGES_DIR%" mkdir "%GH_PACKAGES_DIR%"
+set GH_PACKAGES_DIR=%APPDATA%\Grasshopper\Libraries\
+set GH_PACKAGES_R2020_DIR=%APPDATA%\Grasshopper\Libraries-Inside-Revit-2020\
+set GH_PACKAGES_R2021_DIR=%APPDATA%\Grasshopper\Libraries-Inside-Revit-2021\
+set SAM_DIR=%APPDATA%\SAM\
 
-copy "SAM\*.*" "%GH_PACKAGES_DIR%"
+if not exist "%GH_PACKAGES_DIR%" mkdir "%GH_PACKAGES_DIR%"
+if not exist "%GH_PACKAGES_R2020_DIR%" mkdir "%GH_PACKAGES_R2020_DIR%"
+if not exist "%GH_PACKAGES_R2021_DIR%" mkdir "%GH_PACKAGES_R2021_DIR%"
+if not exist "%SAM_DIR%" mkdir "%SAM_DIR%"
+
+copy "SAM\*.*" "%SAM_DIR%"
+
+echo #Order of files is important or just folder > "%GH_PACKAGES_DIR%SAM.ghlink"
+echo %SAM_DIR% >> "%GH_PACKAGES_DIR%SAM.ghlink"
+
+echo #Order of files is important > "%GH_PACKAGES_R2020_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2020\SAM.Core.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2020_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2020\SAM.Architectural.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2020_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2020\SAM.Analytical.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2020_DIR%SAM_Revit.ghlink"
+
+echo #Order of files is important > "%GH_PACKAGES_R2021_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2021\SAM.Core.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2021_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2021\SAM.Architectural.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2021_DIR%SAM_Revit.ghlink"
+echo %SAM_DIR%Revit 2021\SAM.Analytical.Grasshopper.Revit.gha >> "%GH_PACKAGES_R2021_DIR%SAM_Revit.ghlink"
+
+
+
+
 
