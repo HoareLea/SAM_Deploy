23be86397caafd2675b424bfa3172a822ec04ed6
Will Reynolds <willreynolds@hoarelea.com>
2020-08-19 03:30:25 +0100

    [feature] wait before exiting deploy script

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -1,10 +1,12 @@
 
 @echo off
 set SAM_VER_FLAG=%appdata%\SAM\version
-
+set SAM_TEMP_DIR=%temp%
 echo Checking for latest SAM release...
-curl\curl -s https://api.github.com/repos/HoareLea/SAM_Deploy/releases/latest | grep\grep -o -P "(?<="""tag_name""":\s""").*(?=""")" > %temp%\version
-set /p SAM_VER_NEW=<%temp%\version
+
+
+curl\curl -s https://api.github.com/repos/HoareLea/SAM_Deploy/releases/latest | grep\grep -o -P "(?<="""tag_name""":\s""").*(?=""")" > "%SAM_TEMP_DIR%\samversion"
+set /p SAM_VER_NEW=<"%SAM_TEMP_DIR%\samversion"
 echo Latest SAM version is %SAM_VER_NEW%
 
 if not exist %SAM_VER_FLAG% (
@@ -14,19 +16,20 @@ if not exist %SAM_VER_FLAG% (
 )
 
 if not %SAM_VER_NEW%==%SAM_VER_CUR% (
- echo Yay! Installing new SAM version%SAM_VER_NEW%
+ echo Yay! Installing new SAM version %SAM_VER_NEW%
  echo Downloading...
- wget\wget.exe -N https://github.com/HoareLea/SAM_Deploy/releases/latest/download/SAM.zip
+ wget\wget.exe -N https://github.com/HoareLea/SAM_Deploy/releases/latest/download/SAM.zip -O "%SAM_TEMP_DIR%\SAM.zip"
  echo Extracting...
- 7zip\7za.exe x SAM.zip -oSAM
- pushd SAM
+ 7zip\7za.exe x %SAM_TEMP_DIR%\SAM.zip -bb0 -aoa -o"%SAM_TEMP_DIR%\SAM"
+ pushd "%SAM_TEMP_DIR%\SAM"
  echo Installing...
- install.bat
+ install.bat > "%SAM_TEMP_DIR%\SAMcopy.log"
  popd
  echo %SAM_VER_NEW% > %SAM_VER_FLAG%
- rmdir SAM /S /Q
+ rmdir "%SAM_TEMP_DIR%\SAM" /S /Q
  echo Success! SAM version %SAM_VER_NEW% now installed
 ) else (
  echo Lucky you, installed version %SAM_VER_CUR%is up to date!
 )
 
+timeout 60
