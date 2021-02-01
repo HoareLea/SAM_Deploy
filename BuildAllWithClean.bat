a49ed8d24619258de409963666e979f7167e7119
Will Reynolds <willreynolds@hoarelea.com>
2021-02-01 14:19:28 +0000

    Updated submodules

---
+++ BuildAllWithClean.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set nugetexe=%~dp0nuget.exe
+set msbuildexe=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild
+set CURLexe=curl
+
+:: %CURLexe% --location --output nuget.exe --url https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+
+for /d %%i in (SAM*) do (
+  pushd %%i
+  git clean -dfX
+  echo Pulling %%i
+  git pull
+  :: "%nugetexe%" restore
+  popd
+)
+
+echo Building all SAM Solutions
+"%msbuildexe%" -t:Restore;Rebuild BuildAll_Release.csproj
+
